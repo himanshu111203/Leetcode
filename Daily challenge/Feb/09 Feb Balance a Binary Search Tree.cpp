29c8e1e3914fbae68ad5f061fbb534547ca39845
Himanshu Rohilla <rohillahimanshu42@gmail.com>
2026-02-10 00:36:31 +0530

    Create 09 Feb Balance a Binary Search Tree.cpp
    Given the root of a binary search tree, return a balanced binary search tree with the same node values. If there is more than one answer, return any of them.
    
    A binary search tree is balanced if the depth of the two subtrees of every node never differs by more than 1.
    
     
    
    Example 1:
    
    
    Input: root = [1,null,2,null,3,null,4,null,null]
    Output: [2,1,3,null,null,null,4]
    Explanation: This is not the only correct answer, [3,1,4,null,2] is also correct.
    Example 2:
    
    
    Input: root = [2,1,3]
    Output: [2,1,3]
     
    
    Constraints:
    
    The number of nodes in the tree is in the range [1, 104].
    1 <= Node.val <= 105

---
+++ Daily challenge/Feb/09 Feb Balance a Binary Search Tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root,vector<int>&arr){
+        if(!root) return;
+        inorder(root->left,arr);
+        arr.push_back(root->val);
+        inorder(root->right,arr);
+    }
+    TreeNode* construct(int l,int r,vector<int>&arr){
+        if(l>r) return NULL;
+        int mid=l+(r-l)/2;
+        TreeNode* root=new TreeNode(arr[mid]);
+        root->left=construct(l,mid-1,arr);
+        root->right=construct(mid+1,r,arr);
+        return root;
+    }
+    TreeNode* balanceBST(TreeNode* root) {
+        vector<int>arr;
+        inorder(root,arr);
+        int l=0,r=arr.size()-1;
+        return construct(l,r,arr);
+    }
+};
