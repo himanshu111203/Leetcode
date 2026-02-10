344c35a56abf6e241f0101e72f08ab586b8cf9c4
Himanshu Rohilla <rohillahimanshu42@gmail.com>
2026-02-11 00:14:46 +0530

    Implement longestBalanced function for subarray
    You are given an integer array nums.
    
    A subarray is called balanced if the number of distinct even numbers in the subarray is equal to the number of distinct odd numbers.
    
    Return the length of the longest balanced subarray.
    
     
    
    Example 1:
    
    Input: nums = [2,5,4,3]
    
    Output: 4
    
    Explanation:
    
    The longest balanced subarray is [2, 5, 4, 3].
    It has 2 distinct even numbers [2, 4] and 2 distinct odd numbers [5, 3]. Thus, the answer is 4.
    Example 2:
    
    Input: nums = [3,2,2,5,4]
    
    Output: 5
    
    Explanation:
    
    The longest balanced subarray is [3, 2, 2, 5, 4].
    It has 2 distinct even numbers [2, 4] and 2 distinct odd numbers [3, 5]. Thus, the answer is 5.
    Example 3:
    
    Input: nums = [1,2,3,2]
    
    Output: 3
    
    Explanation:
    
    The longest balanced subarray is [2, 3, 2].
    It has 1 distinct even number [2] and 1 distinct odd number [3]. Thus, the answer is 3.
     
    
    Constraints:
    
    1 <= nums.length <= 1500
    1 <= nums[i] <= 105

---
+++ Daily challenge/Feb/10 Feb 3719. Longest Balanced Subarray I.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int ans=0,n=nums.size();
+        for(int i=0;i<n;i++){
+            unordered_set<int>even;
+            unordered_set<int>odd;
+            for(int j=i;j<n;j++){
+                if(nums[j]%2)
+                odd.insert(nums[j]);
+                else
+                even.insert(nums[j]);
+                if(even.size()==odd.size())
+                ans=max(ans,j-i+1);
+            }
+        }
+        return ans;
+    }
+};
