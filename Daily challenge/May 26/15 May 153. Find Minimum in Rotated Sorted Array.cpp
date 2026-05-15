3938f091af1d8c519fca3c468c30048aaa99f080
Himanshu Rohilla <rohillahimanshu42@gmail.com>
2026-05-15 22:06:37 +0530

    Create 15 May 153. Find Minimum in Rotated Sorted Array.cpp
    Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
    
    [4,5,6,7,0,1,2] if it was rotated 4 times.
    [0,1,2,4,5,6,7] if it was rotated 7 times.
    Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
    
    Given the sorted rotated array nums of unique elements, return the minimum element of this array.
    
    You must write an algorithm that runs in O(log n) time.
    
     
    
    Example 1:
    
    Input: nums = [3,4,5,1,2]
    Output: 1
    Explanation: The original array was [1,2,3,4,5] rotated 3 times.
    Example 2:
    
    Input: nums = [4,5,6,7,0,1,2]
    Output: 0
    Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
    Example 3:
    
    Input: nums = [11,13,15,17]
    Output: 11
    Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
     
    
    Constraints:
    
    n == nums.length
    1 <= n <= 5000
    -5000 <= nums[i] <= 5000
    All the integers of nums are unique.
    nums is sorted and rotated between 1 and n times.

---
+++ Daily challenge/May 26/15 May 153. Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int start=0,end=nums.size()-1,mid,ans;
+        if(nums[0]<=nums[end])
+        return nums[0];
+        while(start<=end){
+            mid=start+(end-start)/2;
+            if(nums[mid]<nums[0]){
+                ans=nums[mid];
+                end=mid-1;
+            }else
+            start=mid+1;
+        }
+        return ans;
+    }
+};
