0c04db48e54dbe10442da440b05aa1313bbd354e
Himanshu Rohilla <rohillahimanshu42@gmail.com>
2026-05-23 17:24:01 +0530

    Create 23 May 1752. Check if Array Is Sorted and Rotated.cpp
    Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
    
    There may be duplicates in the original array.
    
    Note: An array A rotated by x positions results in an array B of the same length such that B[i] == A[(i+x) % A.length] for every valid index i.
    
     
    
    Example 1:
    
    Input: nums = [3,4,5,1,2]
    Output: true
    Explanation: [1,2,3,4,5] is the original sorted array.
    You can rotate the array by x = 2 positions to begin on the element of value 3: [3,4,5,1,2].
    Example 2:
    
    Input: nums = [2,1,3,4]
    Output: false
    Explanation: There is no sorted array once rotated that can make nums.
    Example 3:
    
    Input: nums = [1,2,3]
    Output: true
    Explanation: [1,2,3] is the original sorted array.
    You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
     
    
    Constraints:
    
    1 <= nums.length <= 100
    1 <= nums[i] <= 100

---
+++ Daily challenge/May 26/23 May 1752. Check if Array Is Sorted and Rotated.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool check(vector<int>& arr) {
+        int n=arr.size(),c=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>arr[(i+1)%n])
+            c++;
+            if(c>1) return 0;
+        }
+        return 1;
+    }
+};
